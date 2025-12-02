8b3e470634bfae4b6c3186921303170bb0a4e651
Vinícius Machado de Souza <vinimadesou2@gmail.com>
2025-12-02 19:51:55 -0300

    segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ void main() async {
   await HiveService.init();
 
   await Supabase.initialize(
-    url: '',
-    anonKey: '',
+    url: 'https://gvnxpoxpekfvegactxcg.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2bnhwb3hwZWtmdmVnYWN0eGNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyODY1NjksImV4cCI6MjA3OTg2MjU2OX0.q6o771brWyj9Sdvuc9ZRcYrwmPRVo7w8HGlITfWKhz4',
   );
 
   await NotificationService.initialize();
